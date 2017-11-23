c69698147541cd5031e2e251db950bc433cd33b4
Alistair Cerio <alistair.cerio@gmail.com>
2017-11-23 18:23:39 +0800

    added glide

--- app/src/main/java/com/chipcerio/newsly/features/list/ArticlesAdapter.kt
+++ app/src/main/java/com/chipcerio/newsly/features/list/ArticlesAdapter.kt
@@ -4,8 +4,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import com.chipcerio.newsly.R
+import com.chipcerio.newsly.config.NewslyGlide
 import com.chipcerio.newsly.data.Article
 
 class ArticlesAdapter(private val articles: MutableList<Article>) : RecyclerView.Adapter<ArticlesAdapter.ViewHolder>() {
@@ -20,12 +22,16 @@ class ArticlesAdapter(private val articles: MutableList<Article>) : RecyclerView
             ViewHolder(LayoutInflater.from(parent.context)
                     .inflate(R.layout.item_article, parent, false))
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
+        private var imageView = view.findViewById<ImageView>(R.id.imageView)
         private var titleView = view.findViewById<TextView>(R.id.titleView)
         private var authorView = view.findViewById<TextView>(R.id.authorView)
 
         fun bind(article: Article) {
+            NewslyGlide.with(view.context)
+                    .load(article.urlToImage)
+                    .into(imageView)
             titleView.text = article.title
             authorView.text = article.author
         }
